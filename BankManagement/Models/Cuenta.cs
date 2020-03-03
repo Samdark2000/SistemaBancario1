c41237859fe5631a858ab58048081bb652405fd5
asegesa12 <57462700+asegesa12@users.noreply.github.com>
2020-03-03 19:10:41 -0400

    Modelo

---
+++ BankManagement/Models/Cuenta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Cuenta
+{
+	public Cuenta()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
