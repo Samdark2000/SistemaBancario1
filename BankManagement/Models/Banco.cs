bbb7209ae66d33eee4b49de4e29cf81a36e24cd3
asegesa12 <57462700+asegesa12@users.noreply.github.com>
2020-03-03 21:07:34 -0400

    Login and Register

---
+++ BankManagement/Models/Banco.cs
@@ -0,0 +1,33 @@
+﻿using BankManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Banco
+{
+    [Key]
+    public string Nom { get; set; }
+
+    public decimal Capital { get; set; }
+
+    [Display(Name = "Nombre de clients")]
+    public int NbrClients { get; set; }
+
+    [Display(Name = "Nombre de comptes")]
+    public int NbrComptes { get; set; }
+
+    [Display(Name = "Nombre de crédits")]
+    public int NbrCredits { get; set; }
+
+    [Display(Name = "Somme totale déposée par tous les clients")]
+    public decimal ArgentDepose { get; set; }
+
+    [Display(Name = "Somme totale des crédits")]
+    public decimal SommeCredits { get; set; }
+
+    public ICollection<Cuenta> cuentas { get; set; }
+
+}
---
+++ BankManagement/Models/Cliente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BankManagement.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Direccion { get; set; }
+
+       // public virtual ICollection<Cuenta> Cuentas { get; set; }
+
+    }
+}
+
+
+
+
+  
+
+
