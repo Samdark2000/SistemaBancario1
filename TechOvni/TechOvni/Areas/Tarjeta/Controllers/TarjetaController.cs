c9970410079ab8d1296af9c33a7a945cf99d111b
asegesa12 <57462700+asegesa12@users.noreply.github.com>
2020-03-17 19:46:18 -0400

    Tarjeta

--- TechOvni/TechOvni/Areas/Tarjeta/Controllers/TarjetaController.cs
+++ TechOvni/TechOvni/Areas/Tarjeta/Controllers/TarjetaController.cs
@@ -2,45 +2,76 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TechOvni.Areas.Tarjeta.Models;
+using TechOvni.Controllers;
 using TechOvni.Data;
 using TechOvni.Models;
 
 namespace TechOvni.Areas.Tarjeta.Controllers
 {
+    [Area("Tarjeta")]
+    [Authorize]
     public class TarjetaController : Controller
     {
-        //private LTarjeta _Tarjeta;
-        //private LCategorias lCategorias;
-        //private SignInManager<IdentityUser> _signInManager;
-        //private static DataPaginador<TCursos> models;
-        //private static IdentityError identityError;
-        //private ApplicationDbContext _context;
+        private Ttarjetas _Tarjeta;
+        public LTarjeta lTarjeta;
+        private SignInManager<IdentityUser> _signInManager;
+        private static DataPaginador<Ttarjetas> models;
+        public static IdentityError identityError = null;
 
-        //public CursosController(ApplicationDbContext context, SignInManager<IdentityUser> signInManager, IWebHostEnvironment environment)
-        //{
-        //    _signInManager = signInManager;
-        //    lCategorias = new LCategorias(context);
-        //    _cursos = new LCursos(context, environment);
-        //}
+        public TarjetaController(ApplicationDbContext context, SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            lTarjeta = new LTarjeta(context);
+        }
+        public IActionResult Tarjeta(int id, String Search, int Registros)
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                Object[] objects = new Object[3];
+                var data = lTarjeta.getTCliente(Search);
 
-        //public IActionResult Tarjeta()
-        //{
-        //    if (_signInManager.IsSignedIn(User))
-        //    {
-        //        models = new DataPaginador<Ttarjeta>
-        //        {
-        //            Categoria = lCategorias.getTCategoria(),
-        //            Input = new TCursos()
-        //        };
+                if (0 < data.Count)
+                {
+                    var url = Request.Scheme + "://" + Request.Host.Value;
+                    objects = new LPaginador<Ttarjetas>().paginador(lTarjeta.getTCliente(Search)
+                       , id, Registros, "Cliente", "Cliente", "Cliente", url);
 
-        //        return View(models);
-        //    }
-        //    else
-        //    {
-        //        return Redirect("/Home/Index");
-        //    }
-        //}
+                }
+                else
+                {
+                    objects[0] = "No hay datos que mostrar";
+                    objects[1] = "No hay datos que mostrar";
+                    objects[2] = new List<Ttarjetas>();
+                }
+
+
+
+
+                models = new DataPaginador<Ttarjetas>
+                {
+                    List = (List<Ttarjetas>)objects[2],
+                    Pagi_info = (string)objects[0],
+                    Pagi_navegacion = (string)objects[1],
+
+                    Input = new Ttarjetas()
+                };
+
+                if (identityError != null)
+                {
+                    models.Pagi_info = identityError.Description;
+                    identityError = null;
+                }
+
+                return View(models);
+            }
+            else
+            {
+                return RedirectToAction(nameof(HomeController.Index), "Home");
+            }
+        }
     }
 }
--- TechOvni/TechOvni/Areas/Tarjeta/Models/LTarjeta.cs
+++ TechOvni/TechOvni/Areas/Tarjeta/Models/LTarjeta.cs
@@ -14,34 +14,30 @@ namespace TechOvni.Areas.Tarjeta.Models
     {
         private ApplicationDbContext context;
         private IWebHostEnvironment environment;
-        public LTarjeta(ApplicationDbContext context, IWebHostEnvironment environment)
+
+        public LTarjeta(ApplicationDbContext context)
         {
             this.context = context;
-            this.environment = environment;
-           
         }
 
-        public IdentityError RegistrarCurso(DataPaginador<Ttarjeta> model)
+        public IdentityError RegistrarCliente(Ttarjetas cliente)
         {
             IdentityError identityError;
 
             try
             {
+                if (cliente.TarjetaID.Equals(0))
+                {
+                    context.Add(cliente);
+                }
+                else
+                {
+                    context.Update(cliente);
+                }
 
-                //var curso = new Ttarjeta
-                //{
-                //    Num_Tarjeta = model.Input,
-                //    descripcion = model.Input.descripcion,
-                //    horas = model.Input.horas,
-                //    costo = model.Input.costo,
-                //    estado = model.Input.estado,
-                //    Imagen = imageByte,
-                //    CategoriaID = model.Input.CategoriaID
-                //};
-                //context.Add(curso);
+
                 context.SaveChanges();
                 identityError = new IdentityError { Code = "Done" };
-
             }
             catch (Exception e)
             {
@@ -51,10 +47,79 @@ namespace TechOvni.Areas.Tarjeta.Models
                     Description = e.Message
                 };
 
+
+
             }
 
             return identityError;
+        }
+        public List<Ttarjetas> getTCliente(string valor)
+        {
+            List<Ttarjetas> listCategoria;
 
+            if (valor == null)
+            {
+                listCategoria = context._Ttarjeta.ToList();
+            }
+            else
+            {
+                listCategoria = context._Ttarjeta.Where(c => c.Num_Tarjeta.StartsWith(valor)).ToList();
+            }
+            return listCategoria;
+        }
+
+        internal IdentityError UpdateEstado(int id)
+        {
+            IdentityError identityError;
+
+            try
+            {
+                var cliente = context._Ttarjeta.Where(c => c.TarjetaID.Equals(id)).ToList().ElementAt(0);
+                cliente.estado = cliente.estado? false : true;
+                int data = Convert.ToInt16("a");
+                context.Update(cliente);
+                context.SaveChanges();
+                identityError = new IdentityError { Description = "Done" };
+            }
+            catch (Exception e)
+            {
+                identityError = new IdentityError
+                {
+                    Code = "Error",
+                    Description = e.Message
+                };
+            }
+            return identityError;
+        }
+
+        internal IdentityError DeleteCategoria(int tarjetaID)
+        {
+            IdentityError identityError;
+
+            try
+            {
+                var tarjeta = new Ttarjetas
+                {
+                    TarjetaID = tarjetaID
+                };
+
+                context.Remove(tarjeta);
+                context.SaveChanges();
+
+                identityError = new IdentityError { Description = "Done" };
+
+
+
+            }
+            catch (Exception e)
+            {
+                identityError = new IdentityError
+                {
+                    Code = "Error",
+                    Description = e.Message
+                };
+            }
+            return identityError;
         }
     }
 }
