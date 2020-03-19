0bef41ba5b075344e6b892e735ae9683c9d5aa2c
asegesa12 <57462700+asegesa12@users.noreply.github.com>
2020-03-18 21:16:54 -0400

    Login

---
+++ Tech-Ovni/Tech-Ovni/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tech_Ovni.Data;
+
+[assembly: HostingStartup(typeof(Tech_Ovni.Areas.Identity.IdentityHostingStartup))]
+namespace Tech_Ovni.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
