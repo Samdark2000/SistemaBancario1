4f45ce7378063412309b1953172e1f8689b4091d
samuel encarnacion rosario <samuel20175369@gmail.com>
2020-03-20 12:32:25 -1200

    New

---
+++ TechOvnii/TechOvnii/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechOvnii.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Techh-Onvi/Techh-Onvi/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Techh_Onvi.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
